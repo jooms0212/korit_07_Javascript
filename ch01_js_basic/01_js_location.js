6e3b86e8bf7924ccafa41fc00782e4d8cde1c087
jooms0212 <jooms0212@naver.com>
2025-08-27 17:46:33 +0900

    feat: javascript lesson by function

--- ch01_js_basic/01_js_location.js
+++ ch01_js_basic/01_js_location.js
@@ -1,3 +1,43 @@
 document.write('자바스크립트 js 파일에 위치<br>');
 
-console.log('Hello, JavaScript');
+// console.log('Hello, JavaScript');
+
+  // var x = 5;
+  // var y = 6;
+  // var z = x + y;    // 11이 저장되겠죠.
+  // console.log(z);
+
+//   var x = 7;
+//   z = x + y;      // 13이 저장될겁니다.
+//   console.log(z);
+
+//   console.log(a);
+//   var a = 10;
+//   console.log(a);
+
+// let car;  // 변수 선언
+// console.log(car);
+
+// 객체 선언 및 초기화
+// const person = {
+//   firstName: "Jone",
+//   lastName: "Doe",
+//   age: 20,
+//   eyeColor: "blue",
+// };
+
+// // 객체의 출력
+// console.log(person);
+
+// // 객체의 property 수정 방법 # 1
+// person.firstName = '일';            // 이건 Java에서의 field 수정법인 것 같죠
+// person.lastName = '김';             // access modifier 배우기 이전에요
+// console.log(person);
+
+// // 객체의 property 수정 방법 # 2
+// person['eyeColor'] = '검은색';       // 이건 Java에서의 Map 수정 방법인 것 같죠
+// console.log(person);
+
+let a = 3;
+let b = '3';
+console.log(a==b);
